2cc9ace9758c94fa0000685e0e9d90b7c38b7659
foued05 <amamifoued8@gmail.com>
2022-05-22 03:42:10 +0100

    full stack

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var i = 0;
 var j=0;
-var tab =['Full-Stack Web Developer', 'Software Engineer Student'];
+var tab =['Full-Stack Developer', 'Software Engineer Student'];
 var txt = tab[0];
 var speed = 85;
 document.getElementById("point").innerHTML = '';
